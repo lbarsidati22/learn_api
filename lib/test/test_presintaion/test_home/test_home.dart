c01b42951c9a02994aa6c91bbf558fe10b00af47
lbarsidati22 <lbarsidati575@gmail.com>
2024-11-24 00:59:02 +0000

    nothing today

---
+++ lib/test/test_presintaion/test_home/test_home.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+class TestHome extends StatelessWidget {
+  const TestHome({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('leran api'),
+      ),
+    );
+  }
+}
