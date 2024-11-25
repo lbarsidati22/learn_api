3bf3c1fbb703a4bca42f927774f9531f804b611d
lbarsidati22 <lbarsidati575@gmail.com>
2024-11-25 23:18:52 +0000

    donning enything

--- lib/busnuce/bloc_home/cubit/home_cubit.dart
+++ lib/busnuce/bloc_home/cubit/home_cubit.dart
@@ -1,11 +1,20 @@
 import 'package:bloc/bloc.dart';
 import 'package:equatable/equatable.dart';
+import 'package:learn_api/data/model/result_api.dart';
+import 'package:learn_api/data/ripostery/user_repostiry.dart';
 
 part 'home_state.dart';
 
 class HomeCubit extends Cubit<HomeState> {
-  HomeCubit() : super(HomeInitial());
+  final UserRepostiry userRepostiry;
+  HomeCubit({required this.userRepostiry}) : super(HomeInitial());
   Future<void> getOneUser() async {
-    print('object');
+    print('start getting data');
+    ResultApi resultApi = await userRepostiry.getOneUser(userId: 2);
+    if (resultApi.isDone) {
+      print('done${resultApi.dataOrError}');
+    } else {
+      print('error${resultApi.dataOrError}');
+    }
   }
 }
--- lib/presintation/home/home.dart
+++ lib/presintation/home/home.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:learn_api/busnuce/bloc_home/cubit/home_cubit.dart';
+import 'package:learn_api/data/api/user_api.dart';
+import 'package:learn_api/data/ripostery/user_repostiry.dart';
 import 'package:learn_api/presintation/widget/home_body.dart';
 
 class Home extends StatelessWidget {
@@ -9,7 +11,11 @@ class Home extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return BlocProvider<HomeCubit>(
-      create: (context) => HomeCubit(),
+      create: (context) => HomeCubit(
+        userRepostiry: UserRepostiry(
+          userApi: UserApi(),
+        ),
+      ),
       child: const HomeBody(),
     );
   }
